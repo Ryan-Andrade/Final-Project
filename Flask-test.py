c1a4fd99c1290bc1d14ffc050829c3b1917f3ebe
rsando06 <rsando06@hotmail.com>
2022-05-18 16:46:13 -0700

    added HTML and CSS files for use with Flask

--- Flask-test.py
+++ Flask-test.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World"
+def index():
+    pagetitle = "HomePage"
+    return render_template("index.html",
+                            mytitle=pagetitle,
+                            mycontent="Hello World")
 
 
 if __name__ == "__main__":
