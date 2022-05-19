1b217b309c73e7191137a9d5b74db55220782605
Ryan-Andrade <94945327+Ryan-Andrade@users.noreply.github.com>
2022-05-19 14:35:43 -0700

    I cannot figure out why the ml route does not reinsert the prediction_data. Also we need to add some code to provide functionality in the event of an empty database.

--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,55 @@
 # Dependencies
-import pandas as pd
 import numpy as np
+import pandas as pd
 import yfinance as yf
 import datetime as dt
-import app
+from pymongo import MongoClient
+from collections import Counter
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import confusion_matrix
+from imblearn.over_sampling import RandomOverSampler
+from imblearn.metrics import classification_report_imbalanced
 
-def run_predictions():
-    print("hello")    
+client = MongoClient("mongodb://localhost:27017/")
+db = client.stock_prediction
+collection = db.prediction
+document = collection.find_one()
+ticker = document['ticker']
+
+def preprocessing():
+    data = yf.download(ticker, '2021-01-01', dt.datetime.date(dt.datetime.now()).isoformat(), interval='1d')
+    data.drop(['Adj Close', 'Volume'], axis=1, inplace=True)    
+    data['Day Result'] = np.where(data['Close'] > data['Open'], 1, 0)
+    df = pd.DataFrame(data)
+    df.drop(columns='Close', inplace=True)
+    # Encode the data
+    le = LabelEncoder()
+    df['Open'] = le.fit_transform(df['Open'])
+    df['High'] = le.fit_transform(df['High'])
+    df['Low'] = le.fit_transform(df['Low'])
+    # Split data into training and testing sets
+    X = df.drop(['Day Result'], axis=1)
+    y = df['Day Result'].values
+    # Build the logistic regression model
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    # Scale and normalize the data
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.fit_transform(X_test)
+    # Naive Random Oversampling
+    ros = RandomOverSampler(random_state=0)
+    X_res, y_res = ros.fit_resample(X_train_scaled, y_train)
+    # Train the logistic regression model using resampled data
+    logreg = LogisticRegression(solver='lbfgs', random_state=1)
+    logreg.fit(X_res, y_res)
+    y_pred = logreg.predict(X_test)
+    balanced_accuracy_score(y_test, y_pred)
+    data = {
+        "ticker": ticker,
+        "balanced_accuracy_score": balanced_accuracy_score
+    }
+    return data
--- app.py
+++ app.py
@@ -2,32 +2,35 @@ import json
 from flask import request
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
+from matplotlib import ticker
 import algorithm
 
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/stock_predictions"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/stock_prediction"
 mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    prediction = mongo.db.prediction.find_one()
+    return render_template('index.html', prediction=prediction)
     
 @app.route('/test', methods=['POST'])
 def test():
     output = request.get_json()
     result = json.loads(output)
     print(result)
-    mongo.db.stock_ticker.insert_one({'ticker': result}) # Store result to MongoDB collection
+    mongo.db.prediction.insert_one({}, {'ticker': result}) # Store only one result to MongoDB collection
     return result
 
-#@app.route("/ml")
-#def machine():
-#    ml = mongo.db.ml_vars
-#    ml_data = algorithm.run_predictions()
-#    ml.update_one({}, {"$set":ml_data}, upsert=True)
-#    return redirect('/', code=302)
+@app.route('/ml')
+def logistic_regression():
+    prediction = mongo.db.predcition
+    prediction_data = algorithm.preprocessing()
+    prediction.insert_one({}, {"$set":prediction_data})
+    return redirect('/', code=302)
+
 
 
 
