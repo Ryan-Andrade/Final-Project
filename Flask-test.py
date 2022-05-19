00e78ce11f0304314119ff713702e82f3ffc1402
Ryan-Andrade <94945327+Ryan-Andrade@users.noreply.github.com>
2022-05-18 23:57:42 -0700

    Added functionality that sends the flask variable to python using JSON

--- Flask-test.py
+++ Flask-test.py
@@ -1,14 +1,27 @@
+import json
+
+from flask import request
+
 from flask import Flask, render_template
 
-# Create an instance of Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    pagetitle = "HomePage"
-    return render_template("index.html",
-                            mytitle=pagetitle,
-                            mycontent="Hello World")
+    return render_template('index.html')
+    
+@app.route('/test', methods=['POST'])
+def test():
+    output = request.get_json()
+    print(output) # This is the output that was stored in the JSON within the browser
+    print(type(output))
+    result = json.loads(output) #this converts the json output to a python dictionary
+    print(result) # Printing the new dictionary
+    print(type(result))#this shows the json converted as a python dictionary
+    return result
+
+
+
 
 
 if __name__ == "__main__":
