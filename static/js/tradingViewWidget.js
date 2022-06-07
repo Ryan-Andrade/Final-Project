ebc6daf7c13e9edf5e105472effe49737f89ac1e
Ryan-Andrade <94945327+Ryan-Andrade@users.noreply.github.com>
2022-06-06 21:57:49 -0700

    Made Tradingview widget mobile responsive.

--- static/js/tradingViewWidget.js
+++ static/js/tradingViewWidget.js
@@ -3,27 +3,28 @@ var ticker = document.getElementById('ticker').textContent;
 
 new TradingView.MediumWidget(
     {
-        "symbols": [
-        [
-            ticker
-        ]
-        ],
-        "chartOnly": false,
-        "width": 1000,
-        "height": 500,
-        "locale": "en",
-        "colorTheme": "dark",
-        "isTransparent": false,
-        "autosize": false,
-        "showVolume": false,
-        "hideDateRanges": false,
-        "scalePosition": "right",
-        "scaleMode": "Normal",
-        "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
-        "noTimeScale": false,
-        "valuesTracking": "1",
-        "chartType": "line",
-        "fontColor": "#787b86",
-        "gridLineColor": "rgba(42, 46, 57, 0.06)",
-        "container_id": "tradingview_0a9d5"
-    });
+    "symbols": [
+      [
+        ticker
+      ]
+    ],
+    "chartOnly": false,
+    "width": "100%",
+    "height": 500,
+    "locale": "en",
+    "colorTheme": "dark",
+    "isTransparent": false,
+    "autosize": true,
+    "showVolume": false,
+    "hideDateRanges": false,
+    "scalePosition": "right",
+    "scaleMode": "Normal",
+    "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+    "noTimeScale": false,
+    "valuesTracking": "1",
+    "chartType": "line",
+    "fontColor": "#787b86",
+    "gridLineColor": "rgba(240, 243, 250, 0.06)",
+    "container_id": "tradingview_298c4"
+  }
+    );
